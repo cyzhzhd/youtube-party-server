edb08143b3bfd16797907331bc569f01d4676444
cyzhzhd <cyzhzhd@gmail.com>
2021-01-03 05:45:33 +0000

    add cors on server

--- app.ts
+++ app.ts
@@ -1,14 +1,31 @@
 import express from 'express';
 import logger from 'morgan';
-import path from 'path';
+import cors from 'cors';
 import connect_DB from './src/models/index';
 import indexRouter from './src/routes/index';
 import partyRouter from './src/routes/party';
 
 const app = express();
+
+const allowedOrigins = ['http://localhost:3001', 'https://www.utubeparty.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
 connect_DB();
 app.use('/public', express.static('public'));
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
